e5e73e09502bac6f327de8406cf3cc596bc3a59b
ziqiwangsilvia <wangziqisilvia@hotmail.com>
2017-06-20 14:44:25 +0200

    Add files via upload

--- Lane.py
+++ Lane.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 10:07:16 2017
+
+@author: WangZ
+"""
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
 class Line():
     def __init__(self, img, prev_line = None):
         self.img = img;
@@ -63,9 +73,7 @@ class Line():
         margin = 50
         yvals = np.linspace(0, 100, num=101)*7.2
         yvals = yvals[::-1]
-        # store left and right points
-        left_points = []
-        right_points = []
+        # store left and right point
         nonzero_idx = self.img.nonzero()
         nonzero_x = np.array(nonzero_idx[1])
         nonzero_y = np.array(nonzero_idx[0])
@@ -145,18 +153,30 @@ class Line():
             plt.show()
         return left_fitx, right_fitx, yvals
     
-    def find_lane_and_fit(self, display = False):
+    def find_lane_and_fit(self, prev_left_fitx, prev_right_fitx, prev_yvals, prev_left_fit, prev_right_fit, display = False):
         if self.prev_line == None:
             leftx, lefty, rightx, righty = self.histogram_find_lane()
             left_fitx, right_fitx, yvals = self.fit(leftx, lefty, rightx, righty, run_test=True, display=display)
         else:
             leftx, lefty, rightx, righty = self.smart_find_lane()
-            left_fitx, right_fitx, yvals = self.fit(leftx, lefty, rightx, righty, run_test=True, display=display)
-            if abs(self.lane_width - prev_line.lane_width) > 20 or  self.good_fit_quality()==False:
-                print "smart finding fiailed! Try histogram again!"
-                leftx, lefty, rightx, righty = self.histogram_find_lane()
+            if len(leftx) == 0 or len(rightx) == 0 or len(lefty) == 0 or len(righty) == 0:
+                left_fitx, right_fitx, yvals = prev_left_fitx.copy(), prev_right_fitx.copy(), prev_yvals.copy()
+                self.left_fit, self.right_fit = prev_left_fit.copy(), prev_right_fit.copy()
+            else:
                 left_fitx, right_fitx, yvals = self.fit(leftx, lefty, rightx, righty, run_test=True, display=display)
-        return left_fitx, right_fitx, yvals
+                
+                if abs(self.lane_width - self.prev_line.lane_width) > 20 or  self.good_fit_quality()==False:
+                    print "smart finding fiailed! Try histogram again!"
+                    leftx, lefty, rightx, righty = self.histogram_find_lane()
+                    if len(leftx) == 0 or len(rightx) == 0 or len(lefty) == 0 or len(righty) == 0:
+                        left_fitx, right_fitx, yvals = prev_left_fitx.copy(), prev_right_fitx.copy(), prev_yvals.copy()
+                        self.left_fit, self.right_fit = prev_left_fit.copy(), prev_right_fit.copy()
+                    else:
+                        left_fitx, right_fitx, yvals = self.fit(leftx, lefty, rightx, righty, run_test=True, display=display)
+                        if self.good_fit_quality() == False:
+                            left_fitx, right_fitx, yvals = prev_left_fitx.copy(), prev_right_fitx.copy(), prev_yvals.copy()   
+                            self.left_fit, self.right_fit = prev_left_fit.copy(), prev_right_fit.copy()
+        return left_fitx, right_fitx, yvals, self.left_fit, self.right_fit
             
     def good_fit_quality(self, threshold = 1e-6):
         fit_diff = sum(np.square(self.right_fit[0:-1] - self.left_fit[0:-1]))
